58489d32f614233a4977f01f247db854ed128c06
Nicholas Khami <nicholas.khami@gmail.com>
2023-07-23 19:40:05 -0500

    feature: add warning header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,10 +10,14 @@ import { SectionProvider } from '@/components/SectionProvider'
 export function Layout({ children, sections = [], showFeedbackForm = true }) {
   return (
     <SectionProvider sections={sections}>
+      <div class="flex h-14 items-center justify-center bg-[#a0eee1] text-center font-bold dark:bg-teal-900 dark:text-white lg:h-8">
+        Warning! Vault API is in early alpha and breaking changes will likely
+        occur!
+      </div>
       <div className="h-full lg:ml-72 xl:ml-80">
         <motion.header
           layoutScroll
-          className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex"
+          className="top-14 contents lg:pointer-events-none lg:fixed lg:inset-x-0 lg:top-8 lg:z-40 lg:flex"
         >
           <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pb-8 lg:pt-4 lg:dark:border-white/10 xl:w-80">
             <div className="hidden align-middle lg:flex">
@@ -25,7 +29,10 @@ export function Layout({ children, sections = [], showFeedbackForm = true }) {
                   height={35}
                 />
               </Link>
-              <Link href="/" className="ml-2 mt-1.5 align-middle text-xl font-semibold dark:text-white text-zinc-900">
+              <Link
+                href="/"
+                className="ml-2 mt-1.5 align-middle text-xl font-semibold text-zinc-900 dark:text-white"
+              >
                 Arguflow
               </Link>
             </div>
